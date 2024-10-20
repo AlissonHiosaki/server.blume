251e6034395f4e12046ced51be61177ddc966c6e
Alisson Hiosaki <92666141+AlissonHiosaki@users.noreply.github.com>
2024-10-19 21:27:29 -0300

    Add files via upload

---
+++ src/js/webservice.js
@@ -0,0 +1,20 @@
+
+ /*função de login */
+ function logar(){
+     var login = document.getElementById('login').value;
+     var senha = document.getElementById('senha').value;
+
+     if(login == "admin" && senha == "admin"){
+          location.href = "/home/home.html";
+     }else{
+          /**Alert personalizado */
+          alert('Usuário ou senha incorreta. Tente novamente.');
+          document.getElementById('login').value = "";
+          document.getElementById('senha').value = "";
+          document.getElementById('login').focus(); // foco no input login após o alert
+          return false; // evita que a página seja recarregada
+         
+     }
+ } 
+
+ 
